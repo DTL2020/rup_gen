79113010a7b9105715a400a151a84c41b8af2ba6
DTL2020 <68707763+DTL2020@users.noreply.github.com>
2026-06-09 19:52:39 -0700

    Added bStraight bool for
    non-spiral bended generations and fixed end number of circles larger thickness.

--- rup_gen.cpp
+++ rup_gen.cpp
@@ -23,8 +23,8 @@ struct Face {
 
 // General model params
 
-#define NUM_VERTEX_IN_CIRCLE 25 // one for all diameters define for now, better to do depending on diameter
-#define NUM_STEPS_IN_RUPOR 56 // length in 10ths of mm (centimeter) ?
+#define NUM_VERTEX_IN_CIRCLE 100 // one for all diameters define for now, better to do depending on diameter
+#define NUM_STEPS_IN_RUPOR 25 // length in 10ths of mm (centimeter) ?
 #define SPECIAL_BEGIN 2
 #define SPECIAL_END 2
 float fRadius0 = 0.3f; // start rupor radius. in centimeters.
@@ -35,6 +35,7 @@ float fSpLengthAdvInit = 0.021f; // spiral length advance at each step in X-axis
 float fThickness = 0.1f; // spacing between inner and outer surfaces
 float fThicknessBegin = 0.2f; // spacing between inner and outer surfaces
 float fThicknessEnd = 0.2f; // spacing between inner and outer surfaces
+bool bStraight = true;
 
 // generate circle of vertices around given origin point rotated to given angle
 void CircleGen(std::vector<vec3>& vVerts, vec3 vOrigin, float xAng, float yAng, float zAng, float fRadius)
@@ -164,7 +165,14 @@ int main()
 	// spiral translation position
 	vec3 SpTrans;
 	SpTrans.x = 0.0f;
-	SpTrans.y = (-1.0f) * fSpRadius;
+
+	if (!bStraight)
+	{
+		SpTrans.y = (-1.0f) * fSpRadius;
+	}
+	else
+		SpTrans.y = 0.0f;
+
 	SpTrans.z = 0.0f;
 
 	// create first circle as base start to connect all next
@@ -174,7 +182,12 @@ int main()
 	fRadius = sqrtf(fSquareCurrent / M_PI);
 
 	// translate vOrigin of first circle to start pos
-	vOrigin.x = (-1.0f) * (fThicknessBegin - fThickness);
+	if (!bStraight)
+	{
+		vOrigin.x = (-1.0f) * (fThicknessBegin - fThickness);
+	}
+	else
+	vOrigin.x = 0.0f;
 	vOrigin.y = SpTrans.y;
 	vOrigin.z = SpTrans.z;
 
@@ -200,72 +213,94 @@ int main()
 		// do not spiral rotate (and not spiral axis translate of DIRECT_BEGIN and DIRECT_END number of rupor steps 
 		if (i < SPECIAL_BEGIN) // Z-advance no spiral rot
 		{
-			// spiral transforms
-			// rotate around zero, X-axis, additive at each length step
-			SpTrans = rotatept(SpTrans, deg2rad(fSpAngStep), vec3(1.0, 0.0, 0.0).normalized());
+			if (!bStraight)
+			{
+				// spiral transforms
+				// rotate around zero, X-axis, additive at each length step
+				SpTrans = rotatept(SpTrans, deg2rad(fSpAngStep), vec3(1.0, 0.0, 0.0).normalized());
 
-			// calculate X-step for connecting turns with overlap
-			float fSpLengthAdv = fSpLengthAdvInit * ((fRadius + fThickness) / (fRadius0 + fThickness)); // attempt to be proportional to current circle radius
 
-			// translate origin
-			vOrigin.x += fSpLengthAdv; // to add X-shift in axis of the spiral, step back to additional thickness
-			vOrigin.y = SpTrans.y;
-			vOrigin.z = SpTrans.z;
+				// calculate X-step for connecting turns with overlap
+				float fSpLengthAdv = fSpLengthAdvInit * ((fRadius + fThickness) / (fRadius0 + fThickness)); // attempt to be proportional to current circle radius
 
-			fSpAng += fSpAngStep;
+				// translate origin
+				vOrigin.x += fSpLengthAdv; // to add X-shift in axis of the spiral, step back to additional thickness
+				vOrigin.y = SpTrans.y;
+				vOrigin.z = SpTrans.z;
+
+				fSpAng += fSpAngStep;
+			}
+			else // y only advance
+			{
+				vOrigin.z += fAxisCoordStep;
+			}
 
 			CircleGen(Vertices, vOrigin, fSpAng, 0, 0, fRadius);
 			CircleGen(VerticesOuter, vOrigin, fSpAng, 0, 0, fRadius + fThicknessBegin);
 
 		}
-		else if (i > (NUM_STEPS_IN_RUPOR - SPECIAL_END)) // Y-advance no spiral rot
+		else if (i > (NUM_STEPS_IN_RUPOR - (SPECIAL_END + 1))) // Y-advance no spiral rot
 		{
-			if (bEnd)
+			if (!bStraight)
 			{
-				// adjust step advance at the end once
-				vOrigin.x += (fThicknessEnd - fThickness);
-				bEnd = false;
+				if (bEnd)
+				{
+					// adjust step advance at the end once
+					vOrigin.x += (fThicknessEnd - fThickness);
+					bEnd = false;
+				}
+
+				// spiral transforms
+				// rotate around zero, X-axis, additive at each length step
+				SpTrans = rotatept(SpTrans, deg2rad(fSpAngStep), vec3(1.0, 0.0, 0.0).normalized());
+
+				// calculate X-step for connecting turns with overlap
+				float fSpLengthAdv = fSpLengthAdvInit * ((fRadius + fThicknessEnd) / (fRadius0 + fThicknessEnd)); // attempt to be proportional to current circle radius
+
+				// translate origin
+				vOrigin.x += fSpLengthAdv; // to add X-shift in axis of the spiral, step back to additional thickness
+				vOrigin.y = SpTrans.y;
+				vOrigin.z = SpTrans.z;
+
+				fSpAng += fSpAngStep;
 			}
-
-			// spiral transforms
-			// rotate around zero, X-axis, additive at each length step
-			SpTrans = rotatept(SpTrans, deg2rad(fSpAngStep), vec3(1.0, 0.0, 0.0).normalized());
-
-			// calculate X-step for connecting turns with overlap
-			float fSpLengthAdv = fSpLengthAdvInit * ((fRadius + fThicknessEnd) / (fRadius0 + fThicknessEnd)); // attempt to be proportional to current circle radius
-
-			// translate origin
-			vOrigin.x += fSpLengthAdv; // to add X-shift in axis of the spiral, step back to additional thickness
-			vOrigin.y = SpTrans.y;
-			vOrigin.z = SpTrans.z;
-
-			fSpAng += fSpAngStep;
+			else
+			{
+				vOrigin.z += fAxisCoordStep;
+			}
 
 			CircleGen(Vertices, vOrigin, fSpAng, 0, 0, fRadius);
 			CircleGen(VerticesOuter, vOrigin, fSpAng, 0, 0, fRadius + fThicknessEnd);
 		}
 		else // normal spiral
 		{
-			if (bBegin)
+			if (!bStraight)
 			{
-				// reset begin x-offset once
-				vOrigin.x += (fThicknessBegin - fThickness);
-				bBegin = false;
+				if (bBegin)
+				{
+					// reset begin x-offset once
+					vOrigin.x += (fThicknessBegin - fThickness);
+					bBegin = false;
+				}
+
+				// spiral transforms
+				// rotate around zero, X-axis, additive at each length step
+				SpTrans = rotatept(SpTrans, deg2rad(fSpAngStep), vec3(1.0, 0.0, 0.0).normalized());
+
+				// calculate X-step for connecting turns with overlap
+				float fSpLengthAdv = fSpLengthAdvInit * ((fRadius + fThickness) / (fRadius0 + fThickness)); // attempt to be proportional to current circle radius
+
+				// translate origin
+				vOrigin.x += fSpLengthAdv; // to add X-shift in axis of the spiral
+				vOrigin.y = SpTrans.y;
+				vOrigin.z = SpTrans.z;
+
+				fSpAng += fSpAngStep;
 			}
-
-			// spiral transforms
-			// rotate around zero, X-axis, additive at each length step
-			SpTrans = rotatept(SpTrans, deg2rad(fSpAngStep), vec3(1.0, 0.0, 0.0).normalized());
-
-			// calculate X-step for connecting turns with overlap
-			float fSpLengthAdv = fSpLengthAdvInit * ((fRadius + fThickness) / (fRadius0 + fThickness)); // attempt to be proportional to current circle radius
-
-			// translate origin
-			vOrigin.x += fSpLengthAdv; // to add X-shift in axis of the spiral
-			vOrigin.y = SpTrans.y;
-			vOrigin.z = SpTrans.z;
-
-			fSpAng += fSpAngStep;
+			else
+			{
+				vOrigin.z += fAxisCoordStep;
+			}
 
 			CircleGen(Vertices, vOrigin, fSpAng, 0, 0, fRadius);
 			CircleGen(VerticesOuter, vOrigin, fSpAng, 0, 0, fRadius + fThickness);
